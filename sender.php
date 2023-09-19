fdce032c9eb6b4dae9cfe0f0e114899dc3234feb
Владислав <65068600+MyXaBro@users.noreply.github.com>
2023-09-19 15:38:02 +0300

    sender.php
    Обновил отправителя

--- sender.php
+++ sender.php
@@ -8,15 +8,30 @@ $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
 $channel = $connection->channel();
 
 $channel->queue_declare('file_queue', false, false, false, false);
+$channel->queue_declare('reply_queue', false, false, false, false);
 
-$message = "Меня записали в файл..."; //решил отправить сообщение в файле
+$callback_queue = $channel->queue_declare("", false, false, true, false)[0];
 
-$msg = new AMQPMessage($message);
+$correlation_id = uniqid();
+
+$channel->basic_consume($callback_queue, '', false, true, false, false, function ($response) use ($correlation_id) {
+    if ($response->get('correlation_id') == $correlation_id) {
+        echo "Ответ получен: " . $response->body . "\n";
+    }
+});
+
+$msg = new AMQPMessage(
+    'Запрос на обработку файла',
+    array('correlation_id' => $correlation_id, 'reply_to' => $callback_queue)
+);
+
 $channel->basic_publish($msg, '', 'file_queue');
 
-echo "Сообщение отправлено\n";
+echo "Запрос отправлен\n";
+
+while (count($channel->callbacks)) {
+    $channel->wait();
+}
 
 $channel->close();
 $connection->close();
-?>
-
